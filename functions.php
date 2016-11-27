95f6639d1473a8c10e3236de45419e5bb155c6d9
RKPutra <putra.bossun@gmail.com>
2016-11-27 20:38:47 +0700

    Add files via upload

---
+++ functions.php
@@ -0,0 +1,42 @@
+<?php  
+
+function jajanan(){
+	wp_enqueue_style('style', get_stylesheet_uri());
+}
+
+add_action('wp_enqueue_scripts', 'jajanan');
+
+function wpbootstrap_scripts_with_jquery()
+{
+	// Register the script like this for a theme:
+	wp_register_script( 'custom-script', get_template_directory_uri() . '/bootstrap/js/bootstrap.js', array( 'jquery' ) );
+	// For either a plugin or a theme, you can then enqueue the script:
+	wp_enqueue_script( 'custom-script' );
+}
+add_action( 'wp_enqueue_scripts', 'wpbootstrap_scripts_with_jquery' );
+
+require_once('wp_bootstrap_navwalker.php');
+
+$args = array(
+	'width'         => 1624,
+	'height'        => 700,
+	'default-image' => get_template_directory_uri() . '/images/header.jpg',
+	'uploads'       => true,
+);
+add_theme_support( 'custom-header', $args );
+
+function jajananPasar_setup() {
+	
+	// Navigation Menus
+	register_nav_menus(array(
+		'primary' => __( 'Primary Menu'),
+		'footer' => __( 'Footer Menu'),
+	));
+	
+	// Add featured image support
+	add_theme_support('post-thumbnails');
+	add_image_size('small-thumbnail', 180, 120, true);
+	add_image_size('banner-image', 920, 210, array('left', 'top'));
+}
+
+add_action('after_setup_theme', 'jajananPasar_setup');
