cccdff8fd89121f57fe26f1ed703c7a19fac4734
RKPutra <putra.bossun@gmail.com>
2016-12-25 22:11:38 +0700

    Update page-snack.php

--- page-snack.php
+++ page-snack.php
@@ -2,67 +2,31 @@
 	get_header(); 
 ?>
 <div class="header-user">
-	<h3><b>PILIH SNACK</b></h3>
-	<h5>Pilih minimal 3 snack dari berbagai jenis snack yang tersedia dibawah ini</h5>
+	<h3><b>PILIH MINUMAN</b></h3>
+	<h5>Pilih minuman yang tersedia dibawah ini atau lanjutkan ke bagian pembayaran</h5>
 </div>
 <div class="container-fluid">
 	<div class="row site-row">
 		<div class="col-sm-8 site-overflow">
 			<div class="snack">
 				<div class="col-sm-12">
-					<h4>Lemper</h4>
+					<h4>Air Mineral</h4>
 				</div>
 				<div class="col-sm-3">
 					<img src="<?php echo get_template_directory_uri() . '/asset/images/lemper.jpg'; ?> " class="img-circle" width="100px" height="100px">
-					<h5><b>Lemper Ayam</b></h5>
+					<h5><b>Club Gelas</b></h5>
 					<p>Rp 1.500</p>
 					<input type="button" name="pilih1" value="Pilih snack" class="btn btn-primary"></input>
 				</div>
 				<div class="col-sm-3">
 					<img src="<?php echo get_template_directory_uri() . '/asset/images/lemper.jpg'; ?> " class="img-circle" width="100px" height="100px">
-					<h5><b>Lemper Kepiting</b></h5>
+					<h5><b>Aqua Gelas</b></h5>
 					<p>Rp 1.500</p>
 					<input type="button" name="pilih1" value="Pilih snack" class="btn btn-primary"></input>
 				</div>
 				<div class="col-sm-3">
 					<img src="<?php echo get_template_directory_uri() . '/asset/images/lemper.jpg'; ?> " class="img-circle" width="100px" height="100px">
-					<h5><b>Lemper Ayam</b></h5>
-					<p>Rp 1.500</p>
-					<input type="button" name="pilih1" value="Pilih snack" class="btn btn-primary"></input>
-				</div>
-				
-			</div>
-			<div class="snack">
-				<div class="col-sm-12">
-					<h4>Lemper</h4>
-				</div>
-				<div class="col-sm-3">
-					<img src="<?php echo get_template_directory_uri() . '/asset/images/lemper.jpg'; ?> " class="img-circle" width="100px" height="100px">
-					<h5><b>Lemper Ayam</b></h5>
-					<p>Rp 1.500</p>
-					<input type="button" name="pilih1" value="Pilih snack" class="btn btn-primary"></input>
-				</div>
-				<div class="col-sm-3">
-					<img src="<?php echo get_template_directory_uri() . '/asset/images/lemper.jpg'; ?> " class="img-circle" width="100px" height="100px">
-					<h5><b>Lemper Ayam</b></h5>
-					<p>Rp 1.500</p>
-					<input type="button" name="pilih1" value="Pilih snack" class="btn btn-primary"></input>
-				</div>
-				<div class="col-sm-3">
-					<img src="<?php echo get_template_directory_uri() . '/asset/images/lemper.jpg'; ?> " class="img-circle" width="100px" height="100px">
-					<h5><b>Lemper Ayam</b></h5>
-					<p>Rp 1.500</p>
-					<input type="button" name="pilih1" value="Pilih snack" class="btn btn-primary"></input>
-				</div>
-				<div class="col-sm-3">
-					<img src="<?php echo get_template_directory_uri() . '/asset/images/lemper.jpg'; ?> " class="img-circle" width="100px" height="100px">
-					<h5><b>Lemper Ayam</b></h5>
-					<p>Rp 1.500</p>
-					<input type="button" name="pilih1" value="Pilih snack" class="btn btn-primary"></input>
-				</div>
-				<div class="col-sm-3">
-					<img src="<?php echo get_template_directory_uri() . '/asset/images/lemper.jpg'; ?> " class="img-circle" width="100px" height="100px">
-					<h5><b>Lemper Ayam</b></h5>
+					<h5><b>Aquaria Gelas</b></h5>
 					<p>Rp 1.500</p>
 					<input type="button" name="pilih1" value="Pilih snack" class="btn btn-primary"></input>
 				</div>
@@ -70,46 +34,41 @@
 			</div>
 			<div class="snack">
 				<div class="col-sm-12">
-					<h4>Lemper</h4>
+					<h4>Lain-lain</h4>
 				</div>
 				<div class="col-sm-3">
 					<img src="<?php echo get_template_directory_uri() . '/asset/images/lemper.jpg'; ?> " class="img-circle" width="100px" height="100px">
-					<h5><b>Lemper Ayam</b></h5>
+					<h5><b>The Kotak</b></h5>
 					<p>Rp 1.500</p>
 					<input type="button" name="pilih1" value="Pilih snack" class="btn btn-primary"></input>
 				</div>
 				<div class="col-sm-3">
 					<img src="<?php echo get_template_directory_uri() . '/asset/images/lemper.jpg'; ?> " class="img-circle" width="100px" height="100px">
-					<h5><b>Lemper Ayam</b></h5>
-					<p>Rp 1.500</p>
-					<input type="button" name="pilih1" value="Pilih snack" class="btn btn-primary"></input>
-				</div>
-								
-			</div>
-			<div class="snack">
-				<div class="col-sm-12">
-					<h4>Lemper</h4>
-				</div>
-				<div class="col-sm-3">
-					<img src="<?php echo get_template_directory_uri() . '/asset/images/lemper.jpg'; ?> " class="img-circle" width="100px" height="100px">
-					<h5><b>Lemper Ayam</b></h5>
+					<h5><b>The Sostro Gelas</b></h5>
 					<p>Rp 1.500</p>
 					<input type="button" name="pilih1" value="Pilih snack" class="btn btn-primary"></input>
 				</div>
 				<div class="col-sm-3">
 					<img src="<?php echo get_template_directory_uri() . '/asset/images/lemper.jpg'; ?> " class="img-circle" width="100px" height="100px">
-					<h5><b>Lemper Ayam</b></h5>
+					<h5><b>Nescafe Kotak</b></h5>
 					<p>Rp 1.500</p>
 					<input type="button" name="pilih1" value="Pilih snack" class="btn btn-primary"></input>
 				</div>
 				<div class="col-sm-3">
 					<img src="<?php echo get_template_directory_uri() . '/asset/images/lemper.jpg'; ?> " class="img-circle" width="100px" height="100px">
-					<h5><b>Lemper Ayam</b></h5>
+					<h5><b>Bandrek Gelas</b></h5>
+					<p>Rp 1.500</p>
+					<input type="button" name="pilih1" value="Pilih snack" class="btn btn-primary"></input>
+				</div>
+				<div class="col-sm-3">
+					<img src="<?php echo get_template_directory_uri() . '/asset/images/lemper.jpg'; ?> " class="img-circle" width="100px" height="100px">
+					<h5><b>Bajigur Gelas</b></h5>
 					<p>Rp 1.500</p>
 					<input type="button" name="pilih1" value="Pilih snack" class="btn btn-primary"></input>
 				</div>
 				
 			</div>
+						
 		</div>
 		<div class="col-sm-4">
 			<div class="col-sm-12">
@@ -119,12 +78,12 @@
 				<div id="slider"></div>
 			</div>
 			<div class="col-sm-7">
-				<p>Harga: Rp 1.000 - Rp 10.000</p>
+				<p>Harga: Rp 1.000 - Rp <input type="text" id="amount" readonly style="border:0; width: 50px;"></p>
 			</div>
 			<div class="col-sm-3">
 				<input type="button" value="Filter" class="btn btn-primary filter"></input>
 			</div>
-			<form class="daftar-snack" method="post" action="<?php echo get_page_link( get_page_by_title( 'drink' )->ID ); ?>">
+			<form class="daftar-snack" method="post" action="<?php echo get_page_link( get_page_by_title( 'checkout' )->ID ); ?>">
 				<div class="col-sm-12">
 					<h4><b>Snack Box Anda</b></h4>
 				</div>
@@ -140,13 +99,13 @@
 					<p><b>Harga per Box</b></p>
 				</div>
 				<div class="col-sm-7">
-					<p><b>: Rp 15.500</b></p>
+					<p><b>: Rp 17.000</b></p>
 				</div>
 				<div class="col-sm-5">
 					<p><b>Subtotal</b></p>
 				</div>
 				<div class="col-sm-7">
-					<p><b>: Rp 155.500</b></p>
+					<p><b>: Rp 170.000</b></p>
 				</div>
 				<div class="col-sm-12">
 					<div class="col-sm-3">
@@ -156,18 +115,7 @@
 						<h5>Lemper Ayam</h5>
 					</div>
 					<div class="col-sm-2">
-						<input type="button" value="X"></input>
-					</div>
-				</div>
-				<div class="col-sm-12">
-					<div class="col-sm-3">
-						<img src="<?php echo get_template_directory_uri() . '/asset/images/lemper.jpg'; ?> " class="img-circle" width="50px" height="50px">
-					</div>
-					<div class="col-sm-7">
-						<h5>Lemper Ayam</h5>
-					</div>
-					<div class="col-sm-2">
-						<input type="button" value="X"></input>
+						<span class="glyphicon glyphicon-ok form-control-feedback"></span>
 					</div>
 				</div>
 				<div class="col-sm-12">
@@ -178,14 +126,34 @@
 						<h5>Lemper Ayam</h5>
 					</div>
 					<div class="col-sm-2">
+						<span class="glyphicon glyphicon-ok form-control-feedback"></span>
+					</div>
+				</div>
+				<div class="col-sm-12">
+					<div class="col-sm-3">
+						<img src="<?php echo get_template_directory_uri() . '/asset/images/lemper.jpg'; ?> " class="img-circle" width="50px" height="50px">
+					</div>
+					<div class="col-sm-7">
+						<h5>Lemper Ayam</h5>
+					</div>
+					<div class="col-sm-2">
+						<span class="glyphicon glyphicon-ok form-control-feedback"></span>
+					</div>
+				</div>
+				<div class="col-sm-12">
+					<div class="col-sm-3">
+						<img src="<?php echo get_template_directory_uri() . '/asset/images/lemper.jpg'; ?> " class="img-circle" width="50px" height="50px">
+					</div>
+					<div class="col-sm-7">
+						<h5>Aqua Gelas</h5>
+					</div>
+					<div class="col-sm-2">
 						<input type="button" value="X"></input>
 					</div>
 				</div>
-				<div class="col-sm-11 site-tambah">
-					<h5>Tambah Snack</h5>
-				</div>
+				
 				<div class="col-sm-11">
-					<button type="submit" class="btn btn-primary">Selesai Pilih Snack</button>
+					<button type="submit" class="btn btn-primary">Selesai Pilih Minuman</button>
 				</div>
 			</form>
 		</div>
@@ -195,4 +163,3 @@
 <?php 
 	get_footer();
 
-?>
