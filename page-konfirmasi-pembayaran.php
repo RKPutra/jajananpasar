79bfed5a420705ba34c8c9ddcdcf65b1a839fd51
RKPutra <putra.bossun@gmail.com>
2016-12-30 19:55:31 +0700

    Update page-konfirmasi-pembayaran.php

--- page-konfirmasi-pembayaran.php
+++ page-konfirmasi-pembayaran.php
@@ -6,13 +6,14 @@
 	<h5>Isi data berikut untuk bukti pembayaran</h5>
 </div>
 <div class="container-fluid site-form-pembayaran">
-	<form method="post">
+	<form method="post" data-toggle="validator" role="form" >
 		<div class="row input-pembayaran">
 			<div class="col-sm-2">
 				<label>Nama</label>
 			</div>
 			<div class="col-sm-8">
-				<input type="text" class="form-control"></input>
+				<input type="text" class="form-control" required></input>
+				<div class="help-block with-errors"></div>
 			</div>
 		</div>
 		<div class="row input-pembayaran">
@@ -20,7 +21,8 @@
 				<label>No. Rekening</label>
 			</div>
 			<div class="col-sm-8">
-				<input type="text" class="form-control"></input>
+				<input type="text" class="form-control" required></input>
+				<div class="help-block with-errors"></div>
 			</div>
 		</div>
 		<div class="row input-pembayaran">
@@ -28,7 +30,8 @@
 				<label>Nama Bank</label>
 			</div>
 			<div class="col-sm-8">
-				<input type="text" class="form-control"></input>
+				<input type="text" class="form-control" required></input>
+				<div class="help-block with-errors"></div>
 			</div>
 		</div>
 		
@@ -37,7 +40,8 @@
 				<label>Tgl. Kirim</label>
 			</div>
 			<div class="col-sm-8 ">
-				<input type="text" class="input-group date form_date form-control" data-date="" data-date-format="dd MM yyyy" data-link-field="dtp_input2" data-link-format="yyyy-mm-dd">				
+				<input type="text" class="input-group date form_date form-control" data-date="" data-date-format="dd MM yyyy" data-link-field="dtp_input2" data-link-format="yyyy-mm-dd" required>
+				<div class="help-block with-errors"></div>				
 			</div>
 			
 		</div>
@@ -46,7 +50,8 @@
 				<label>Jam Kirim</label>
 			</div>
 			<div class="col-sm-5">
-				<input type="text" class="input-group date form_time form-control" data-date="" data-date-format="hh:ii" data-link-field="dtp_input3" data-link-format="hh:ii"></input>
+				<input type="text" class="input-group date form_time form-control" data-date="" data-date-format="hh:ii" data-link-field="dtp_input3" data-link-format="hh:ii" required></input>
+				<div class="help-block with-errors"></div>
 			</div>
 			<div class="col-sm-2 wib">
 				<label class="control-label">WIB</label>
@@ -57,7 +62,8 @@
 		</div>
 		
 		
-		<input type="button" value="Proses Sekarang" class="btn btn-primary form-control button-pembayaran"></input>
+		
+		<button type="submit" class="btn btn-primary form-control button-pembayaran">Proses Sekarang</button>
 	</form>
 </div>
 
